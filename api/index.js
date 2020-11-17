0672c41f15d6d6a2093454748fe2cdc2b573739e
tan <ttru702@gmail.com>
2020-11-17 18:49:42 +0900

    install express-validator

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser')
 const app = require('express')()
+const { body, validatorResult } = require('express-validator')
 
 app.use(bodyParser.json())
-app.get('/get', (req, res) => {
+
+app.get('/get_test', (req, res) => {
   res.json({ data: 'data' })
 })
 
