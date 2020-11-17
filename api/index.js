2721a0a2252b4c3455c1151359ce5fb855e10f48
tan <ttru702@gmail.com>
2020-11-17 19:33:42 +0900

    test express-validator get request

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 const bodyParser = require('body-parser')
 const app = require('express')()
-const { body, validatorResult } = require('express-validator')
+const { query, validationResult } = require('express-validator')
 
 app.use(bodyParser.json())
 
-app.get('/get_test', (req, res) => {
-  res.json({ data: 'data' })
+app.get('/test_get',[
+  query('q').isString().isLength({min: 8, max: 8})
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  res.json({
+    query: req.query
+  })
 })
 
 module.exports = app
