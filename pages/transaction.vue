4ce419d08d3a15d2d6b28fa2b0a354d29d66d2c1
tan <ttru702@gmail.com>
2020-11-23 21:47:08 +0900

    test load transactions

---
+++ pages/transaction.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{this.$store.state.transactions}}</div>
+</template>
+
+<script>
+export default {
+  async asyncData({ store }) {
+    await store.dispatch('load_transactions')
+  }
+}
+</script>
