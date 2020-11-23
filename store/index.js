5ca33ecadd0c6e30570f2fab827c0071d81ab360
tan <ttru702@gmail.com>
2020-11-23 21:43:30 +0900

    implement load transaction  in vuex

--- store/index.js
+++ store/index.js
@@ -1,10 +1,14 @@
 export const state = () => ({
-  products: []
+  products: [],
+  transactions: []
 })
 
 export const mutations = {
   set_products(state, products) {
     state.products = products
+  },
+  set_transactions(state, transactions) {
+    state.transactions = transactions
   }
 }
 
@@ -13,5 +17,10 @@ export const actions = {
     const endpoint = 'http://localhost:3000/api/product'
     const { result } = await this.$http.$get(endpoint)
     commit('set_products', result)
+  },
+  async load_transactions({ commit }) {
+    const endpoint = 'http://localhost:3000/api/transaction'
+    const { result } = await this.$http.$get(endpoint)
+    commit('set_transactions', result)
   }
 }
