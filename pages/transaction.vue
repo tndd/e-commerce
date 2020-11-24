432620c0ca9ce35edf62086d84906cf136439c54
tan <ttru702@gmail.com>
2020-11-24 11:46:23 +0900

    implement transactions table

--- pages/transaction.vue
+++ pages/transaction.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>{{this.$store.state.transactions}}</div>
+  <div>
+    <div>
+      <h1>Transactions</h1>
+      <table border="1">
+        <tbody>
+          <tr>
+            <th>Ordered Date</th>
+            <th>ID</th>
+            <th>Buyer ID</th>
+            <th>Product ID</th>
+            <th>Quantity</th>
+          </tr>
+          <tr v-for="t in transactions" :key="t.ordered_date">
+            <td>{{t.ordered_date}}</td>
+            <td>{{t.id}}</td>
+            <td>{{t.buyer_id}}</td>
+            <td>{{t.product_id}}</td>
+            <td>{{t.quantity}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div>
+      <h1>Regist Transactions</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   async asyncData({ store }) {
     await store.dispatch('load_transactions')
+  },
+  computed: {
+    transactions() {
+      return this.$store.state.transactions
+    }
   }
 }
 </script>
