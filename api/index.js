aa882ac71a4bc186925edbbf13dc3694ef55a1ff
tan <ttru702@gmail.com>
2020-11-25 23:41:19 +0900

    update table col "id"

--- api/index.js
+++ api/index.js
@@ -156,7 +156,7 @@ app.post('/transaction', [
       quantity: product.quantity
     }
     const payload_tran_progress = {
-      id,
+      transaction_id: id,
       update_date: ordered_date
     }
     queries.push(mysql.format(query_transaction, payload_transaction))
