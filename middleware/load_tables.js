98f21fa4bd8f1583f0ef33f7d8b377a1d6598591
tan <ttru702@gmail.com>
2020-11-27 20:42:45 +0900

    add load_transaction_progress
    candidates
    = id_pg
    = {...id_pg}
    = Object.assign({}, state.transaction_progress, id_pg)

--- middleware/load_tables.js
+++ middleware/load_tables.js
@@ -2,7 +2,8 @@ export default async ({ store }) => {
   try {
     await Promise.all([
       store.dispatch('load_products'),
-      store.dispatch('load_transactions')
+      store.dispatch('load_transactions'),
+      store.dispatch('load_transaction_progress')
     ])
   }
   catch(e) {
--- store/index.js
+++ store/index.js
@@ -1,6 +1,7 @@
 export const state = () => ({
   products: [],
   transactions: [],
+  transaction_progress: {},
   cart: []
 })
 
@@ -40,6 +41,13 @@ export const mutations = {
   },
   clear_cart_items(state) {
     this.state.cart = []
+  },
+  set_transaction_progress(state, progresses) {
+    let id_pg = {}
+    progresses.forEach(p => {
+      id_pg[p.id] = p.status
+    })
+    state.transaction_progress = {...id_pg}
   }
 }
 
@@ -48,6 +56,11 @@ export const actions = {
     const endpoint = 'http://localhost:3000/api/product'
     const { result } = await this.$http.$get(endpoint)
     commit('set_products', result)
+  },
+  async load_transaction_progress({ commit }) {
+    const ep_tran_progress = 'http://localhost:3000/api/transaction_progress'
+    const { result } = await this.$http.$get(ep_tran_progress)
+    commit('set_transaction_progress', result)
   },
   async load_transactions({ commit }) {
     const endpoint = 'http://localhost:3000/api/transaction'
