eab69153d6061904bf029268956e8e08a38832e3
tan <ttru702@gmail.com>
2020-11-29 17:25:56 +0900

    implement load product_inventory & product_version

--- store/index.js
+++ store/index.js
@@ -1,5 +1,7 @@
 export const state = () => ({
   products: [],
+  product_inventory: {},
+  product_version: {},
   transactions: [],
   transaction_progress: {},
   cart: []
@@ -55,14 +57,48 @@ export const mutations = {
       id_pg[p.transaction_id] = p.status
     })
     state.transaction_progress = {...id_pg}
+  },
+  set_product_inventory(state, inventories) {
+    let product_inventory = {}
+    inventories.forEach(i => {
+      product_inventory[i.product_id] = {
+        update_date: i.update_date,
+        num: i.inventory
+      }
+    })
+    state.product_inventory = {...product_inventory}
+  },
+  set_product_version(state, versions) {
+    let product_version = {}
+    versions.forEach(v => {
+      product_version[v.product_id] = {
+        update_date: v.update_date,
+        name: v.name,
+        price: v.price,
+        description: v.description
+      }
+    })
+    state.product_version = product_version
   }
 }
 
 export const actions = {
-  async load_products({ commit }) {
+  async load_product_inventory({ commit }) {
+    const endpoint = 'http://localhost:3000/api/product_inventory'
+    const { result } = await this.$http.$get(endpoint)
+    commit('set_product_inventory', result)
+  },
+  async load_product_version({ commit }) {
+    const endpoint = 'http://localhost:3000/api/product_inventory'
+    const { result } = await this.$http.$get(endpoint)
+    commit('set_product_version', result)
+  },
+  async load_products({ commit, dispatch }) {
     const endpoint = 'http://localhost:3000/api/product'
     const { result } = await this.$http.$get(endpoint)
     commit('set_products', result)
+    await dispatch('load_product_inventory')
+    await dispatch('load_product_version')
   },
   async load_transaction_progress({ commit }) {
     const ep_tran_progress = 'http://localhost:3000/api/transaction_progress'
