25bb240802adedda157c89f22199715c1b5c8f1f
tan <ttru702@gmail.com>
2020-11-30 21:24:54 +0900

    update create table "product_version"

--- api/sql/product_version/create.sql
+++ api/sql/product_version/create.sql
@@ -1,11 +1,9 @@
 CREATE TABLE `product_version` (
-  `no` int unsigned NOT NULL AUTO_INCREMENT,
-  `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'this represents a version of product.',
   `product_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'this represents a version of product.',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `price` int unsigned NOT NULL,
   `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  PRIMARY KEY (`no`),
-  KEY `product_version_FK` (`product_id`),
+  PRIMARY KEY (`product_id`,`update_date`),
   CONSTRAINT `product_version_FK` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
