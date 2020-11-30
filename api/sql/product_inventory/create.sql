05149a400d64fed4e9549c1fc73018944fe222be
tan <ttru702@gmail.com>
2020-11-30 21:14:12 +0900

    update create table "product_inventory"
    composite key

--- api/sql/product_inventory/create.sql
+++ api/sql/product_inventory/create.sql
@@ -1,9 +1,7 @@
 CREATE TABLE `product_inventory` (
-  `no` int unsigned NOT NULL AUTO_INCREMENT,
-  `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `product_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'product original id',
-  `inventory` int unsigned DEFAULT NULL,
-  PRIMARY KEY (`no`),
-  KEY `product_inventory_FK` (`product_id`),
+  `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `inventory` int unsigned DEFAULT NULL COMMENT 'null means disabled product.',
+  PRIMARY KEY (`product_id`,`update_date`),
   CONSTRAINT `product_inventory_FK` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
