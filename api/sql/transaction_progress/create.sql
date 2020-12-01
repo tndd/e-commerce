21c2369950723eeb5e89c3f67274685ed4bf3164
tan <ttru702@gmail.com>
2020-12-01 13:36:55 +0900

    update tables

--- api/sql/transaction_progress/create.sql
+++ api/sql/transaction_progress/create.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `transaction_progress` (
   `transaction_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status_id` int unsigned NOT NULL,
+  `status` varchar(32) NOT NULL,
   `description` text,
   PRIMARY KEY (`transaction_id`,`update_date`),
   KEY `transaction_progress_FK` (`transaction_id`),
-  KEY `transaction_progress_FK_1` (`status_id`),
+  KEY `transaction_progress_FK_1` (`status`),
   CONSTRAINT `transaction_progress_FK` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `transaction_progress_FK_1` FOREIGN KEY (`status_id`) REFERENCES `transaction_status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `transaction_progress_FK_1` FOREIGN KEY (`status`) REFERENCES `transaction_status` (`status`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- api/sql/transaction_status/create.sql
+++ api/sql/transaction_status/create.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `transaction_status` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `registrant_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
