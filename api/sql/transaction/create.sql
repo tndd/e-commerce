b2d915bd776a224b1d9094ca0cc18a71de88d0e9
tan <ttru702@gmail.com>
2020-12-01 19:32:50 +0900

    add foreign key buyer_id

--- api/sql/transaction/create.sql
+++ api/sql/transaction/create.sql
@@ -7,5 +7,7 @@ CREATE TABLE `transaction` (
   `quantity` int unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `transaction_FK` (`product_id`),
-  CONSTRAINT `transaction_FK` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `transaction_FK_1` (`buyer_id`),
+  CONSTRAINT `transaction_FK` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `transaction_FK_1` FOREIGN KEY (`buyer_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
