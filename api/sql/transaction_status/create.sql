b39d94af40bf4aadc42682a2e1fc3f3f5b6fddf2
tan <ttru702@gmail.com>
2020-12-01 13:13:40 +0900

    add transaction_status

---
+++ api/sql/transaction_status/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `transaction_status` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `registrant_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
---
+++ api/sql/transaction_status/insert.sql
@@ -0,0 +1,1 @@
+INSERT INTO `e-commerce`.transaction_status SET ?;
