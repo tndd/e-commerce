873742e9e1a0cae690937f063361f12116d3ad89
tan <ttru702@gmail.com>
2020-12-01 17:36:18 +0900

    add foreign key "user_id" in product table

--- api/sql/product/create.sql
+++ api/sql/product/create.sql
@@ -2,5 +2,7 @@ CREATE TABLE `product` (
   `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `registrated_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `registrant_user_id` varchar(36) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `product_FK` (`registrant_user_id`),
+  CONSTRAINT `product_FK` FOREIGN KEY (`registrant_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
