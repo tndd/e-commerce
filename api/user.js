e7bfd07f6fc326f8593f6c3f6cbec584af3ca759
tan <ttru702@gmail.com>
2020-12-02 21:15:49 +0900

    add api select user version

--- api/user.js
+++ api/user.js
@@ -19,4 +19,15 @@ app.get('/user', async (req, res) => {
   }
 })
 
+app.get('/user_version', async (req, res) => {
+  const query = read_sql('./api/sql/user_version/select.sql')
+  const [status, response] = await execute_query(query)
+  if (status) {
+    res.json(response)
+  }
+  else {
+    res.status(400).json(response)
+  }
+})
+
 module.exports = app
